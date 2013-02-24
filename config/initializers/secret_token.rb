1a2a31dd7bacb584e79bbb0a14b8d8c502d406c1
asaf ahi-mordehai <asafam@asafs-MacBook-Air.local>
2013-02-25 00:29:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NextplayForms::Application.config.secret_token = 'ae7e54d55bece3984b6413af88b89e792b453c9dec4aedb757cab476a8425ac1dd63049ee5a5d212d690a473a3e2d1c34c731af19ea3e8480092c7168b048f47'
